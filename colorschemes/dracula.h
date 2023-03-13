c2ccc0d7eed22b2cdda956199062f194711cbe0a
Diwas10 <rimaldiwas10@gmail.com>
2023-03-13 19:56:43 +0545

    Update colorscheme

--- colorschemes/dracula.h
+++ colorschemes/dracula.h
@@ -1,14 +1,17 @@
 /* Dracula colorscheme for dmenu */
 
-static const char base[]           = "#1e1e2e";
-static const char rosewater[]      = "#f5e0dc";
-static const char maroon[]         = "#eba0ac";
+static const char darkbg[]	= "#1f1f2e";
+static const char white[]	= "#f8f8f2";
+static const char pink[]	= "#FF79C6";
+static const char cyan[]	= "#8be9fd";
+static const char yellow[]	= "#f1fa8c";
+static const char purple[]	= "#bd93f9";
 
 static const char *colors[SchemeLast][2] = {
 	                        /*   fg     bg     */
-	[SchemeNorm]          = { rosewater, base },
-	[SchemeSel]           = { maroon, base },
-	[SchemeNormHighlight] = { rosewater,   base },
-	[SchemeSelHighlight]  = { maroon, "#45454e" },
-	[SchemeOut]           = { "#00ff00", "#00ff00" },
+	[SchemeNorm]          = { white,	darkbg },
+	[SchemeSel]           = { cyan,		darkbg },
+	[SchemeNormHighlight] = { white,	darkbg },
+	[SchemeSelHighlight]  = { cyan,		"#45454e" },
+	[SchemeOut]           = { "#00ff00","#00ff00" },
 };
